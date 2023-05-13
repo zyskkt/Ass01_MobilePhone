4e41ccdae554b19fea4d40bcd5de2df1f68a81eb
Kent Zysk <kent@kentzysk.com>
2023-05-12 23:23:33 -0500

    Finished

--- Ass01_Mobile_Pone/src/Test.java
+++ Ass01_Mobile_Pone/src/Test.java
@@ -1,17 +1,19 @@
 public class Test {
     public static void main(String[] args) {
         // Create phones
-        Phone.createPhone("123456789012345");
-        Phone.createPhone("987654321098765");
+        Phone.createPhone("358844102814380");
+        Phone.createPhone("352233097803515");
 
         // Power on the first phone by calling the Phone class and getting the phone by IMEI
-        Phone firstPhone = Phone.getPhoneByIMEI("123456789012345");
-
+        Phone firstPhone = Phone.getPhoneByIMEI("358844102814380");
+        Phone secondPhone = Phone.getPhoneByIMEI("352233097803515");
             firstPhone.togglePower();
 
         // Make calls
-        Phone.makeCall("123456789012345", "*#060#");
-        Phone.makeCall("123456789012345", "2024561414");
-        Phone.makeCall("987654321098765", "2024561414");
+        Phone.makeCall("358844102814380", "8675309");
+        Phone.makeCall("352233097803515", "2024561414");
+        secondPhone.togglePower();
+        Phone.makeCall("352233097803515", "2024561414");
+        Phone.makeCall("352233097803515", "*#060#");
     }
 }
